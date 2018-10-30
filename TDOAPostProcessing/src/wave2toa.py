b993d729dd0a42633fc4a0ad06929e6bdd513637
Roi Toledano <rtoledano@apple.com>
2018-10-30 08:54:27 +0200

    only filtering signals by speaker

--- TDOAPostProcessing/src/wave2toa.py
+++ TDOAPostProcessing/src/wave2toa.py
@@ -12,7 +12,7 @@ from termcolor import colored
 import src.algorithms as algos
 from math import cos, sin, pi
 from utils import UTILS
-from utils import SignalHandler as sigs
+from utils import SignalHandler
 
 plotting = True
 
@@ -45,7 +45,7 @@ def resampling(speaker,rec_Fs):
 class recwav(object):
     def __init__(self):
         self.path = '../inputs/first_test.wav'
-        self.sample_rate = 192000
+        self.sample_rate = 44100
         self.toa_csv_path = '../output/toa_record_'+str(int(time.time()))+'.csv'
         self.results_path = '../output/locations_results_'+str(int(time.time()))+'.csv'
         self.record_sig = []
@@ -65,7 +65,7 @@ class recwav(object):
 
         # If Stereo
         if self.num_of_channels == 2:
-            print 'Our Application support only mono files'
+            print 'Our Application support only mono files , record from one input only'
             sys.exit(0)
 
         self.rec_time = np.linspace(0, len(self.signal) / self.sample_rate , num=len(self.signal))
@@ -177,28 +177,22 @@ class Speaker(object):
         self.z = 0
         self.chirp = []
         self.params = {}
+        self.proccessed_signal = SignalHandler()
+        self.unfiltered_signal = {}
 
-    def BuildChirp(self):
-        Fs = 192000
+    def BuildChirp(self, freqs_dict, Fs):
         tt = np.linspace(0,0.001, Fs*0.001)
         matlabchirps = io.loadmat('../inputs/all_chirp.mat')
         chirps = matlabchirps['allchirp']
-        if self.id == 1:
-            tmpchirp = signal.chirp(tt, 42000, 0.001, 37000)
-            nrm = np.linspace(1, 0.02, num=len(tmpchirp))
-            return np.multiply(nrm, tmpchirp) , chirps[:, 0]
-        elif self.id == 2:
-            tmpchirp = signal.chirp(tt,30000,0.001,35000)
-            nrm = np.linspace(1, 0.02, num=len(tmpchirp))
-            return np.multiply(nrm, tmpchirp) , chirps[:, 1]
-        elif self.id == 3:
-            tmpchirp = signal.chirp(tt,23000,0.001,28000)
-            nrm = np.linspace(1, 0.02,num=len(tmpchirp))
-            return np.multiply(nrm,tmpchirp), chirps[:, 2]
-        elif self.id == 4:
-            tmpchirp = signal.chirp(tt, 21000, 0.001, 16000)
-            nrm = np.linspace(1, 0.02, num=len(tmpchirp))
-            return np.multiply(nrm, tmpchirp), chirps[:, 3]
+        self.unfiltered_signal = {'Fs': Fs, 'low_freq': freqs_dict[str(self.id)][1],
+                                  'high_freq': freqs_dict[str(self.id)][0], 'chirp_time': 0.001}
+
+        tmpchirp = signal.chirp(tt, self.unfiltered_signal['high_freq'],
+                                self.unfiltered_signal['chirp_time'],
+                                self.unfiltered_signal['low_freq'])
+        self.unfiltered_signal['normalized_vect'] = np.linspace(1, 0.02, num=len(tmpchirp))
+        sig1 = np.multiply(self.unfiltered_signal['normalized_vect'], tmpchirp), chirps[:, self.id - 1]
+        return sig1
 
     def WaveformBuilder(self):
         if (('type' not in self.params) | ('Fs' not in self.params) | ('f0' not in self.params) | ('f1' not in self.params) | ('t1' not in self.params) | ('nrm' not in self.params) | ('mode' not in self.params)):
@@ -219,34 +213,43 @@ class Speaker(object):
 
             return final_sig
 
-    def Define_ID(self,my_id):
-        # define all params , generate signals for correlation, load matlab TX signals
+    def Define_ID_WFB(self,my_id):
         self.id = my_id
-        # self.params['type'] = 'builtin'  # { builtin , unique }
-        # self.params['t1'] = 0.001
-        # self.params['Fs'] = 88200
-        # self.params['mode'] = 'quadratic'  # { logarithmic, linear, hyperbolic , quadratic }
-        # self.params['nrm'] = False
-        # if self.id == 1:
-        #     self.params['f0'] = 30000
-        #     self.params['f1'] = 27500
-        # elif self.id == 2:
-        #     self.params['f0'] = 15000
-        #     self.params['f1'] = 12500
-        # elif self.id == 3:
-        #     self.params['f0'] = 20000
-        #     self.params['f1'] = 17500
-        # elif self.id == 4:
-        #     self.params['f0'] = 25000
-        #     self.params['f1'] = 22500
-        #
-        # self.chirp = self.WaveformBuilder()
-        #
-        # matlabchirps = io.loadmat('inputs/all_chirp.mat')
-        # chirps = matlabchirps['allchirp']
-        # self.matlab_chirp = chirps[:,self.id]
+        self.params['type'] = 'builtin'  # { builtin , unique }
+        self.params['t1'] = 0.001
+        self.params['Fs'] = 88200
+        self.params['mode'] = 'quadratic'  # { logarithmic, linear, hyperbolic , quadratic }
+        self.params['nrm'] = False
+        if self.id == 1:
+            self.params['f0'] = 30000
+            self.params['f1'] = 27500
+        elif self.id == 2:
+            self.params['f0'] = 15000
+            self.params['f1'] = 12500
+        elif self.id == 3:
+            self.params['f0'] = 20000
+            self.params['f1'] = 17500
+        elif self.id == 4:
+            self.params['f0'] = 25000
+            self.params['f1'] = 22500
 
-        self.chirp, self.matlab_chirp = self.BuildChirp()
+        self.chirp = self.WaveformBuilder()
+
+        matlabchirps = io.loadmat('inputs/all_chirp.mat')
+        chirps = matlabchirps['allchirp']
+        self.matlab_chirp = chirps[:,self.id]
+
+    def Define_ID(self,my_id, freqs_dict, Fs):
+        '''
+        define all params , generate signals for correlation, load matlab TX signals ,
+        filters record following to chirp parameters
+        :param my_id: speaker ID
+        :param freqs_dict: all expected chirps frequencies
+        :param Fs: recording sample_rate.
+        :return:
+        '''
+        self.id = my_id
+        self.chirp, self.matlab_chirp = self.BuildChirp(freqs_dict, Fs)
 
     def DefineLocation(self,op,x=0.0,y=0.0,z=0.0):
         if op == 'csv':
@@ -267,15 +270,25 @@ if __name__ == '__main__':
 
     correl = []
     sp_list = [Speaker(), Speaker(), Speaker(), Speaker()]
-    # filtered_recs = [sigs(), sigs(), sigs(), sigs()]
+    speakers_frequencies = {'1': [42000, 37000],
+                            '2': [35000, 30000],
+                            '3': [28000, 23000],
+                            '4': [21000, 16000]}
+    speakers_locations_d = {'1': [0.0, 0.0, 2.30],
+                            '2': [3.9, 0.0, 2.25],
+                            '3': [0.65, 3.8, 2.23],
+                            '4': [3.9, 4.04, 1.52]}
+    Fs = 192000
 
     # for item in sp_list:
     #     item.DefineLocation('manual')
 
-    sp_list[0].DefineLocation('s', 0.0, 0.0, 2.30)
-    sp_list[1].DefineLocation('s', 3.9, 0.0, 2.25)
-    sp_list[2].DefineLocation('s', 0.65, 3.8, 2.23)
-    sp_list[3].DefineLocation('s', 3.9, 4.04, 1.52)
+    # sp_list[0].DefineLocation('s', 0.0, 0.0, 2.30)
+    # sp_list[1].DefineLocation('s', 3.9, 0.0, 2.25)
+    # sp_list[2].DefineLocation('s', 0.65, 3.8, 2.23)
+    # sp_list[3].DefineLocation('s', 3.9, 4.04, 1.52)
+
+
     algorithm = int(raw_input("choose algorithm:\n\t(1) Chan Algorithm\n\t"
                               "(2) Taylor Algorithm\n\t(3) Room LUT Algorithm\n\t"
                               "(4) All Algorithms").strip())
@@ -284,35 +297,55 @@ if __name__ == '__main__':
     record = recwav()
     record.change_path('/Users/roitoledano/Music/Logic/newfreqs.wav','in')
     record.PlotSignal('one by one.wav')
-    # record.PlotPartSig('second.wav')
     record.PlotFFT('one by one.wav')
-    # record.plotOnlyOneCycle()
-    record.Spectogram()
+    # record.Spectogram()
 
+    # define speaker parameters and filtering signals according to speakers frequencies
     for i in range(len(sp_list)):
-        sp_list[i].Define_ID(i+1)
+        sp_list[i].Define_ID(i+1, speakers_frequencies, Fs)
+        sp_list[i].DefineLocation('s',
+                                  speakers_locations_d[str(sp_list[i].id)][0],
+                                  speakers_locations_d[str(sp_list[i].id)][1],
+                                  speakers_locations_d[str(sp_list[i].id)][2])
         resampling(sp_list[i], record.sample_rate)
+        sp_list[i].unfiltered_signal['signal'] = record.signal
+        sp_list[i].unfiltered_signal['time_vect'] = record.rec_time
+        sp_list[i].proccessed_signal.defineParams(sp_list[i].unfiltered_signal)
+        sp_list[i].proccessed_signal.BPF(139)
+
+    if plotting:
+        print "plotting"
+        # plt.figure(789)
+        # plt.plot(sp_list[0].unfiltered_signal['time_vect'], sp_list[i].unfiltered_signal['signal'])
+        # for sp in sp_list:
+        #     plt.plot(sp.proccessed_signal.record_time,
+        #              sp.proccessed_signal.filtered_signal[:len(sp.proccessed_signal.record_time)])
+        # plt.grid()
+        # plt.show()
+        # fftd = {}
+        # freqsd = {}
+        # plt.figure(790)
+        # for sp in sp_list:
+        #     fftd[str(sp.id)] = fftpkt.fftshift(fftpkt.fft(sp.proccessed_signal.filtered_signal))
+        #     freqsd[str(sp.id)] = fftpkt.fftshift(
+        #         fftpkt.fftfreq(len(sp.proccessed_signal.filtered_signal), 1.0 / 192000))
+        #     plt.plot(freqsd[str(sp.id)], abs(fftd[str(sp.id)]))
+        #
+        # plt.show()
 
     with open(record.toa_csv_path, 'wb') as fout:
         writer = csv.DictWriter(fout, fieldnames=["iteration","toa_sp_1", "toa_sp_2", "toa_sp_3",
                                                   "toa_sp_4", "corr_1","corr_2","corr_3","corr_4"])
         writer.writeheader()
         iterr = 1
-        # while(record.curr_time < record.total_time):
-        #     record.CutCurrSig()
-        #     corr_list = utils_obj.CorrWith4(record,sp_list)
-        #     toa , max_corr = utils_obj.FindTOA(corr_list,record)
-        #
-        #     writer.writerow({"iteration":iterr,"toa_sp_1":toa[0], "toa_sp_2":toa[1], "toa_sp_3":toa[2],
-        #                      "toa_sp_4":toa[3],"corr_1":max_corr[0],"corr_2":max_corr[1],
-        #                      "corr_3":max_corr[2],"corr_4":max_corr[3]})
-        #     iterr += 1
-        # generate peaks vector
+
         peaks, _ = signal.find_peaks(record.signal, height=int(0.7*(max(record.signal))), distance=100)
-        plt.plot(record.signal)
-        plt.plot(peaks, record.signal[peaks], "x")
-        plt.plot(np.zeros_like(record.signal), "--", color="gray")
-        plt.show()
+        if plotting:
+            plt.plot(record.signal)
+            plt.plot(peaks, record.signal[peaks], "x")
+            plt.plot(np.zeros_like(record.signal), "--", color="gray")
+            plt.show()
+
 
         for i in range(len(peaks)):
             # record.CutCurrSig()
